5a9c9e0ebedcc9238ad3aff3c046b4e5ad01070b
icegreener <icegreener@users.noreply.github.com>
2020-09-28 13:09:51 +0300

    Al
    script

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public class Robot
+    {
+
+
+        public static int weght = 0;
+        public static int strange = 0;
+        public static int speed = 0;
+
+        public Robot()
+        {
+
+        }
+
+        public void Debugs()
+        {
+            Debug.Log(weght);
+            Debug.Log(strange);
+            Debug.Log(speed);
+
+        }
+
+        public void Rand()
+        {
+            weght = Random.Range(0, 10);
+            strange = Random.Range(0, 10);
+            speed = Random.Range(0, 10);
+        }
+    }
+
+    void Start()
+    {
+        Robot robot = new Robot();
+        robot.Debugs();
+        robot.Rand();
+        robot.Debugs();
+
+
+    }
+
+    
+
+}
